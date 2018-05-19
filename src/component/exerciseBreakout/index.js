752f5287bf9bac5bbd92aa19d2a43062a0ebba10
Alan Ngo <>
2018-05-19 15:41:30 -0700

    Changed Navbar and Breakout

--- src/component/exerciseBreakout/index.js
+++ src/component/exerciseBreakout/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from '../nav/index';
-// import './input-area.scss';
+import './breakout.scss';
 
 class exerciseBreakout extends React.Component {
   constructor(props) {
@@ -14,30 +14,39 @@ class exerciseBreakout extends React.Component {
 
   render() {
     return (
-      <div className="exerciseBreakout" >
-        <Nav />
-        <div className="exerciseName">
-          <div className="exerciseIcon">
-            <img src="" />
-          </div>
-          <div className="exercise">
-            <h1>{this.props.location.state.name}</h1>
-          </div>
-        </div>
+        <div className="exerciseBreakout">
+            <Nav />
+            <div className="exerciseName">
+                <ul class="exUL">
+                    <h1 class="title">Exercise</h1>
+                    <li class="exLI">
+                        <div className="exerciseContent">
+                            <div className="exerciseIcon">
+                                <img src={this.props.location.state.img} />
+                            </div>
+                            <div className="exercise">
+                                <h1>{this.props.location.state.name}</h1>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="exLI">
+                        <div className="leaderboard">
+                            <h1>Leaderboard</h1>
+                            <ul className="leaderboardContent">
+                                <li>#1: {this.props.location.state.leaderBoard[0]}</li>
+                                <li>#2: {this.props.location.state.leaderBoard[1]}</li>
+                                <li>#3: {this.props.location.state.leaderBoard[2]}</li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
 
-        <div className="steps">
-          <h1>Video</h1>
-          <iframe src={this.props.location.state.video}></iframe>
+            <div className="steps">
+                <h1>How To:</h1>
+                <iframe width="840" height="472.5" src="https://www.youtube.com/embed/dzYvUgpqB8s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
         </div>
-
-        <div className="leaderboard">
-          <ul>
-            <li>{this.props.location.state.leaderBoard[0]}</li>
-            <li>{this.props.location.state.leaderBoard[1]}</li>
-            <li>{this.props.location.state.leaderBoard[2]}</li>
-          </ul>
-        </div>
-      </div>
     );
   }
 }
--- src/component/nav/index.js
+++ src/component/nav/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import * as routes from '../routes';
 import {Link} from 'react-router-dom';
-// import './input-area.scss';
+import './nav.scss';
 
 class Nav extends React.Component {
 
   render() {
     return (
       <div className="nav">
+        <span>Gamigen</span>
+        <div className="navStuff">
         <Link to={routes.CONTENT} className="home">Home</Link>
         <Link to={routes.PROFILE} className="profile">Profile</Link>
+        </div>
       </div>
     );
   }
